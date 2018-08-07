23bf4879281bd6f007b23fd0f6091fbddcf35498
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-08-07 15:54:44 +0200

    ref(schematics:console): uppercase log messages

--- packages/schematics/src/lib/console-messages/match.ts
+++ packages/schematics/src/lib/console-messages/match.ts
@@ -1,7 +1,7 @@
 import { tags, terminal as color } from '@angular-devkit/core';
 
 export function Match(message: string) {
-  return tags.oneLine`  ${color.bgGreen(color.black('match'))} ${message}`;
+  return tags.oneLine`  ${color.bgGreen(color.black('MATCH'))} ${message}`;
 }
 
 
--- packages/schematics/src/lib/console-messages/warning.ts
+++ packages/schematics/src/lib/console-messages/warning.ts
@@ -1,5 +1,5 @@
 import { tags, terminal as color } from '@angular-devkit/core';
 
 export function Warning(message: string) {
-  return tags.oneLine`  ${color.bgYellow(color.black('warning'))} ${message}`;
+  return tags.oneLine`  ${color.bgYellow(color.black('WARNING'))} ${message}`;
 }
