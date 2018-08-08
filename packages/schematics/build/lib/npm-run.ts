d9966a3f558938ad023271317b17cb4df483ab38
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-08-08 15:38:29 +0200

    ref(schmatics:build): use spawn instead of exec - This allows to read the output from the started child-process

--- packages/schematics/build/lib/npm-run.ts
+++ packages/schematics/build/lib/npm-run.ts
@@ -1,42 +1,10 @@
-import { exec } from 'child_process';
+import { spawn } from 'child_process';
 
 export function npmRun(script: string) {
-  return new Promise((resolve, reject) => {
-    exec(`npm run ${script}`, (err, stdout, stderr) => {
-      const error = readErr(err, stderr);
-
-      return !!error ? reject(error) : maybeLog(stdout).then(() => resolve());
-    });
-  });
-}
-
-/**
- * Currently a check is needed for stderr.
- * Jest outputs to stderr even when there is no error.
- * (see: https://github.com/facebook/jest/issues/5064)
- *
- * @param err
- * @param stderr
- */
-function readErr(err: Error|null, stderr: string) {
-  const errorRegex = /\b()\b/
-  if (err) {
-    console.log('ERR', err);
-    return err;
-  } else if (stderr.includes('err')) {
-    console.log('STDERR', stderr);
-    return stderr;
-  }
-
-  return null;
-}
-
-function maybeLog(message: string) {
-  return new Promise(resolve => {
-    if (message) {
-      console.log(message);
-    }
-
-    return resolve();
-  });
+  return new Promise((resolve, reject) =>
+    spawn('npm', ['run', script], {
+      stdio: 'inherit',
+      shell: true
+    }).on('close', exitCode => (exitCode === 0 ? resolve() : reject()))
+  );
 }
