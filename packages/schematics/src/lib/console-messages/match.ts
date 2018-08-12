202abcfeca543073b0bc48da2d704f5d6969dfb0
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-08-12 16:06:48 +0200

    fix: adjust path to angular devkit

--- packages/schematics/src/lib/console-messages/match.ts
+++ packages/schematics/src/lib/console-messages/match.ts
@@ -1,4 +1,4 @@
-import { tags, terminal as color } from '@angular-devkit/schematics/node_modules/@angular-devkit/core';
+import { tags, terminal as color } from '@angular-devkit/core';
 
 export function Match(message: string) {
   return tags.oneLine`  ${color.bgGreen(color.black('MATCH'))} ${message}`;
